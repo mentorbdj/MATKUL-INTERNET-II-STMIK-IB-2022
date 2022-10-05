bc3ea272f0a72fbb7b23027f3e26d9de8f9236bd
Luthfy on Infinix <projectmanager@gmail.coom>
2022-10-05 10:49:31 +0800

    update materi praktikum aritmatika

---
+++ praktikum_aritmatika/praktikum_penjumlahan.php
@@ -0,0 +1,10 @@
+<h1>Praktikum Penjumlahan</h1>
+<form action="proses_penjumlahan.php" method="post">
+    <label for="angka1">Masukan Angka 1</label>
+    <input type="number" name="angka_1" id="angka1" required>
+    <br>
+    <label for="angka2">Masukan Angka 2</label>
+    <input type="number" name="angka_2" id="angka2" required>
+    <br>
+    <input type="submit" value="Hitung">
+</form>
---
+++ praktikum_aritmatika/proses_penjumlahan.php
@@ -0,0 +1,10 @@
+<?php
+
+$angkaSatu = $_POST['angka_1'];
+$angkaDua  = $_POST['angka_2'];
+
+$hasil     = $angkaSatu + $angkaDua;
+
+echo "<h1>Penjumlahan dari $angkaSatu dengan $angkaDua adalah " . $hasil . "</h1>";
+
+?>
