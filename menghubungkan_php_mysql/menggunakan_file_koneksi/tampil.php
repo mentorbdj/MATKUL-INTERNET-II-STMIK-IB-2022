9556d73eec58c59fb858e2eaafa5bb731d66d5d2
Luthfy E. Permana <36814098+Luthfy@users.noreply.github.com>
2022-10-28 11:05:10 +0800

    Update tampil.php
    penambahan tampil data

--- menghubungkan_php_mysql/menggunakan_file_koneksi/tampil.php
+++ menghubungkan_php_mysql/menggunakan_file_koneksi/tampil.php
@@ -1,5 +1,31 @@
 <?php 
 
  include 'koneksi.php';
+ 
+ // lakukan query
+ $query  = "SELECT * FROM tb_user";
+ $result = $koneksi->query($query);
+ 
+?>
+
+<!-- ekstrak data -->
+<!-- tampilkan data -->
+<table border="1">
+    <tr>
+        <th>ID</th>
+        <th>Username</th>
+        <th>Password</th>
+        <th>Created At</th>
+    </tr>
+    <?php while ($fetch = $result->fetch_array()) : ?>
+        <tr>
+            <td><?= $fetch['id'] ?? '-' ?></td>
+            <td><?= $fetch['username'] ?? '-' ?></td>
+            <td><?= $fetch['password'] ?? '-' ?></td>
+            <td><?= $fetch['created_at'] ?? '-' ?></td>
+        </tr>
+    <?php endwhile ?>
+</table>
+
 
  
